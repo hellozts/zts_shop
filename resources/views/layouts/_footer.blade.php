fe70c3143b16c0c77918e843e4a3ca73e0ade1e7
hellozts <hellozts@gmail.com>
2020-06-24 09:17:45 +0800

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="http://github.com/hellozts" target="_blank">Hellozts</a>
+            编码和设计 <span style="color: #e27575;font-size: 14px">❤</span>
+        </p>
+        <p class="float-right">
+            <a href="mailto:hellozts2@gmail.com">联系我们</a>
+        </p>
+    </div>
+</footer>
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">ZTS Shop</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+            <span class="navbar-toggle-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav navbar-right">
+                <li class="nav-item"><a class="nav-link" href="#">登陆</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
