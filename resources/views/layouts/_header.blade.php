c2c18590c763d4e55bf9476fb5e436f9285871f1
hellozts <hellozts@gmail.com>
2020-06-26 20:15:45 +0800

    初始化

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,15 +1,26 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">ZTS Shop</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
-            <span class="navbar-toggle-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto"></ul>
-            <ul class="navbar-nav navbar-right">
-                <li class="nav-item"><a class="nav-link" href="#">登陆</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
-            </ul>
-        </div>
+        <ul class="navbar-nav navbar-right">
+            @guest
+            <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登陆</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+            @else
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown">
+                    <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60"
+                         class="img-responsiv img-circle" width="30px" height="30px" />
+                    {{ Auth::user()->name }}
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" id="logout" href="#"
+                       onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </div>
+            </li>
+            @endguest
+        </ul>
     </div>
 </nav>
