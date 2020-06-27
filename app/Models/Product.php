3a9f2876cc9274849656a15828441a56a1fecc4b
hellozts <hellozts@gmail.com>
2020-06-27 15:54:05 +0800

    产品模型

---
+++ app/Models/Product.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Product
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property string $image
+ * @property bool $on_sale
+ * @property float $rating
+ * @property int $sold_count
+ * @property int $review_count
+ * @property float $price
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\ProductSku[] $skus
+ * @property-read int|null $skus_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereOnSale($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereRating($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereReviewCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereSoldCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Product extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'on_sale',
+        'rating',
+        'sold_count',
+        'review_count',
+        'price'
+    ];
+
+    protected $casts = [
+        'on_sale' => 'boolean'
+    ];
+
+    public function skus() {
+        return $this->hasMany(ProductSku::class);
+    }
+}
